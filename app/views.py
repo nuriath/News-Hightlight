47f3df3307475d9a038aa803c500dd48210e75bb
nuriath <mnuriath28@gmail.com>
2019-02-15 17:20:07 +0200

    create news.html

--- app/views.py
+++ app/views.py
@@ -9,4 +9,13 @@ def index():
     View root page function that returns the index page and its data
     '''
     
-    return render_template('index.html',)
+    return render_template('index.html')
+
+@app.route('/news/<int:movie_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
+
