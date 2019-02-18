173c72c25494bb58e2c1c54f22155baf82e86d9a
nuriath <mnuriath28@gmail.com>
2019-02-18 16:25:52 +0200

    add a macros in index.html

--- app/views.py
+++ app/views.py
@@ -15,8 +15,6 @@ def index():
     entertainment_news = get_news('entertainment')
     business_news = get_news('business')
     sport_news = get_news('sport')
-
-    print(entertainment_news, business_news, sport_news)
     
     return render_template('index.html', title = title,  entertainment = entertainment_news, business =business_news, sport = sport_news)
 
